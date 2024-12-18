2ab6ff57ec5ecc54571e809c708d2a72ad272b22
Houssien Zeineddine <houessienzd@hotmail.com>
2024-12-18 10:39:39 +0200

    JS <> adding the variables i may need

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+const postfixCalculator = document.getElementById("postFix");
+const prefixCalculator = document.getElementById("preFix");
+const calculator = document.getElementById("calculator");
+
+const buttons = document.getElementsByClassName("numbers");
+const operators = document.getElementsByClassName("operators");
+const equals = document.getElementById("equals");
+const clear = document.getElementById("clear");
+const del = document.getElementById("delete");
+
+const input = document.getElementById("input");
+const output = document.getElementById("output");
